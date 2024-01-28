3d728ff63cfa2af06b0a14ad528042910da5508e
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2024-01-28 13:49:25 +1300

    hurray, it works now

---
+++ keybinder/src/configure_windows.ps1
@@ -0,0 +1,22 @@
+# Remove Task View
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name ShowTaskViewButton -Value 0 -Type DWord -Force
+# Remove Widgets
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name TaskbarDa -Value 0 -Type DWord -Force
+# Remove Co-Pilot
+New-Item -Path "HKCU:\Software\Policies\Microsoft\Windows" -Name WindowsCopilot
+Set-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\Windows\WindowsCopilot" -Name TurnOffWindowsCopilot -Value 1 -Type DWord -Force
+# Align Centre
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name TaskbarAl -Value 0 -Type DWord -Force
+# Set Search Icon To Hidden
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -Name SearchboxTaskbarMode -Value 0 -Force
+
+# Disable Windows Web Search
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -Name "BingSearchEnabled" -Value 0
+
+# Turn Off Hibernation
+powercfg -h off
+
+# Restore Full Right-Click Menu
+New-Item -Path "HKCU:\Software\Classes\CLSID" -Name "{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}"
+New-Item -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" -Name InprocServer32
+Set-ItemProperty -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" -Name "(Default)" -Value 0
--- keybinder/src/main.rs
+++ keybinder/src/main.rs
@@ -1,3 +1,4 @@
+use powershell_script;
 use std::process::Command;
 use winapi::um::winuser::GetAsyncKeyState;
 
@@ -7,6 +8,8 @@ fn main() {
     let edge = "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe";
     // Folders
     let local_repo = "C:\\Local";
+    // Script
+    let script_cleanup = include_str!("configure_windows.ps1");
     // Arguments
     let edge_profile_personal = "--profile-directory=Default";
     let edge_profile_work = "--profile-directory=Profile 4";
@@ -14,29 +17,36 @@ fn main() {
     let _edge_personal = ["alt", "q"];
     let _edge_work = ["alt", "q"];
 
+    // Core Keylogger
     loop {
 
+        let enter = unsafe { GetAsyncKeyState(0x0D) };
         let alt = unsafe { GetAsyncKeyState(0x12) };
         let q = unsafe { GetAsyncKeyState(0x51) };
         let w = unsafe { GetAsyncKeyState(0x57) };
-        // let e = unsafe { GetAsyncKeyState(0x45) };
-        // let r = unsafe { GetAsyncKeyState(0x52) };
+        let _e = unsafe { GetAsyncKeyState(0x45) };
+        let _r = unsafe { GetAsyncKeyState(0x52) };
         let a = unsafe { GetAsyncKeyState(0x41) };
+        let c = unsafe { GetAsyncKeyState(0x43) };
+        let n = unsafe { GetAsyncKeyState(0x4E) };
+        let m = unsafe { GetAsyncKeyState(0x4D) };
 
         if alt != 0 && q != 0 {
-            open_application(edge, edge_profile_personal);
-            println!("Opened: Edge-Personal");
+            run_application(edge, edge_profile_personal);
+            println!("Running: Edge-Personal");
         }
-        if alt != 0 && w != 0 {
-            open_application(edge, edge_profile_work);
-            println!("Opened: Edge-Work");
+        else if alt != 0 && w != 0 {
+            run_application(edge, edge_profile_work);
+            println!("Running: Edge-Work");
         }
-        if alt != 0 && a != 0 {
+        else if alt != 0 && a != 0 {
             open_folder(local_repo);
             println!("Opened: Local Folder");
         }
-
-        
+        else if alt != 0 && c != 0 && n != 0 && m != 0 && enter != 0 {
+            println!("Running: Windows Configuration Script");
+            run_powershell(script_cleanup)
+        }
         
         // Lets not overload the CPU
         std::thread::sleep(std::time::Duration::from_millis(100));
@@ -45,12 +55,22 @@ fn main() {
 }
 
 
-fn open_application<'a>(app: &'a str, arg: &str) -> &'a str<> {
+fn run_application<'a>(app: &'a str, arg: &str) -> &'a str<> {
     let _output = Command::new(app)
     .arg(arg)
     .output();
 
     return app;
+}
+fn run_powershell(script: &str) {
+    match powershell_script::run(script) {
+        Ok(output) => {
+            println!("{}", output);
+        }
+        Err(e) => {
+            eprintln!("Error: {}", e);
+        }
+    }
 }
 fn open_folder(folder: &str) -> &str {
     let _output = Command::new("explorer")
